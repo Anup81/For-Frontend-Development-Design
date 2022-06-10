fc723f5f4ce8ad2f46a93201434e52476115c33e
Anup81 <anupdhakal81@gmail.com>
2022-06-10 16:12:05 +0545

    Styling Movie App

--- src/features/store.js
+++ src/features/store.js
@@ -1,6 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import moviesReducer from "./movies/movieSlice";
+// import logger from "redux-logger";
 
 export const store = configureStore({
-  reducer: moviesReducer,
+  reducer: { movies: moviesReducer },
 });
