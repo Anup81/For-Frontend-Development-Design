0aa65bf514ccbc2e50a8f99a25d1ae3ac224145d
Anup81 <anupdhakal81@gmail.com>
2022-06-10 16:33:31 +0545

    Making use of Create Async Thunk

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,27 +1,26 @@
 import React, { useEffect } from "react";
 import "./Home.scss";
 import MovieListing from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/MovieApi";
-import { APIkey } from "../../common/apis/MovieApiKey";
+// import movieApi from "../../common/apis/MovieApi";
+// import { APIkey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../features/movies/movieSlice";
+import { fetchAsyncMovies } from "../../features/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
-        .catch((err) => {
-          console.log("Error :", err);
-        });
-      dispatch(addMovies(response.data));
-      // console.log("The response from API", response);
-    };
+    // const fetchMovies = async () => {
+    // const response = await movieApi
+    //   .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
+    //   .catch((err) => {
+    //     console.log("Error :", err);
+    //   });
+    // dispatch(addMovies(response.data));
+    // console.log("The response from API", response);
+    // };
 
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+  }, [dispatch]);
 
   return (
     <div>
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,4 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import movieApi from "../../common/apis/MovieApi";
+import { APIkey } from "../../common/apis/MovieApiKey";
+
+export const fetchAsyncMovies = createAsyncThunk(
+  "movies/fetchAsyncMovies",
+  async () => {
+    const movieText = "Harry";
+    const response = await movieApi
+      .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
+      .catch((err) => {
+        console.log("Error :", err);
+      });
+    return response.data;
+  }
+);
 
 const initialState = {
   movies: {},
@@ -10,6 +25,17 @@ const movieSlice = createSlice({
   reducers: {
     addMovies: (state, { payload }) => {
       state.movies = payload;
+    },
+  },
+
+  extraReducers: {
+    [fetchAsyncMovies.pending]: () => console.log("Pending"),
+    [fetchAsyncMovies.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Successfully");
+      return { ...state, movies: payload };
+    },
+    [fetchAsyncMovies.rejected]: () => {
+      console.log("Rejected!");
     },
   },
 });
